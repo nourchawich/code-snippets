0ebede9c51722b591f30c7a12f86709330c18b37
Nour Chawich <1257310+nourchawich@users.noreply.github.com>
2018-12-06 14:58:25 +0100

    Add snippets ListView

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Snippet
+
+
+class SnippetList(ListView):
+    template_name = 'snippets/snippet_list.html'
+    context_object_name = 'snippets'
+
+    def get_queryset(self):
+        qs = Snippet.objects.all()
+        # In the case this view was triggered by a visit to /snippets/tags/TAG/
+        # self.kwargs['tag'] will have the tag. We use it to filter the queryset
+        if 'tag' in self.kwargs:
+            qs = qs.filter(tags__contains=[self.kwargs['tag']])
+        return qs.order_by('-created_at')
